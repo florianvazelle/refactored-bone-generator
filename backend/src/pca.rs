d1380952e268a3d145468568c16c9ae0b8f171d2
florianvazelle <ponythugflorian@gmail.com>
2021-09-26 11:40:48 +0200

    fix ci

--- backend/src/pca.rs
+++ backend/src/pca.rs
@@ -1,8 +1,8 @@
-pub use nalgebra::Vector3;
 use nalgebra::Matrix3;
+pub use nalgebra::Vector3;
 use ordered_float::OrderedFloat;
 
-fn compute_barycenter(data: &Vec<Vector3<f64>>) -> Vector3<f64> {
+fn compute_barycenter(data: &[Vector3<f64>]) -> Vector3<f64> {
     let mut center = Vector3::new(0.0, 0.0, 0.0);
 
     for value in data {
@@ -15,12 +15,12 @@ fn compute_barycenter(data: &Vec<Vector3<f64>>) -> Vector3<f64> {
     center
 }
 
-fn transform_space(axis: &Vector3<f64>, data: &Vec<Vector3<f64>>) -> Vec<Vector3<f64>> {
+fn transform_space(axis: &Vector3<f64>, data: &[Vector3<f64>]) -> Vec<Vector3<f64>> {
     data.iter().map(|value| value - axis).collect()
 }
 
-fn compute_covmatrix(data: &Vec<Vector3<f64>>) -> Matrix3<f64> {
-    let points_count = data.len() as f64;  // nombre de points dans le nuage de points
+fn compute_covmatrix(data: &[Vector3<f64>]) -> Matrix3<f64> {
+    let points_count = data.len() as f64; // nombre de points dans le nuage de points
 
     // On trouve la moyenne en x, y et z
     let mut average = Vector3::new(0.0, 0.0, 0.0);
@@ -54,7 +54,7 @@ fn power_method(m: &Matrix3<f64>, num_simulations: Option<i32>) -> (f64, Vector3
 
     // 2) Pour k de 0, 1, 2 ...
     for _ in 0..num_simulations {
-        let v_normalized = v.normalize();  // On normalise V
+        let v_normalized = v.normalize(); // On normalise V
 
         // a) On calcule M . Vk
         let m_dot_v = m * v_normalized;
@@ -74,32 +74,39 @@ fn inv(point: &Vector3<f64>) -> Vector3<f64> {
     Vector3::new(-point[0], -point[1], -point[2])
 }
 
-pub fn compute_bones_generation(members: &Vec<(String, Vec<Vector3<f64>>)>) -> Vec<(String, Vec<Vector3<f64>>)> {
-    let mut principal_components: Vec<(String, Vec<Vector3<f64>>)> = Vec::with_capacity(members.len());
+pub fn compute_bones_generation(
+    members: &[(String, Vec<Vector3<f64>>)],
+) -> Vec<(String, Vec<Vector3<f64>>)> {
+    let mut principal_components: Vec<(String, Vec<Vector3<f64>>)> =
+        Vec::with_capacity(members.len());
 
     // Pour tout les membres
     for (key, points_cloud) in members.iter() {
-
         // ----- STEP 1 -----
 
         // a) On calcule le barycentre des données
-        let barycenter = compute_barycenter(&points_cloud);
+        let barycenter = compute_barycenter(points_cloud);
+
         // b) On centre les données
-        let center_points_cloud = transform_space(&barycenter, &points_cloud);  // to local space
+        let center_points_cloud = transform_space(&barycenter, points_cloud); // to local space
+
         // c) On calcule la matrice de covariance
         let cov_matrix = compute_covmatrix(&center_points_cloud);
 
         // ----- STEP 2 -----
 
         // d) On calcule les valeurs propres (eigenvalue), et les vecteurs propres (eigenvector)
-        let (eigenvalue, eigenvector) = power_method(&cov_matrix, Some(100));  // eigendecomposition of covariance matrix
+        let (eigenvalue, eigenvector) = power_method(&cov_matrix, Some(100)); // eigendecomposition of covariance matrix
         println!("eigenvalue: {} | eigenvector: {}", eigenvalue, eigenvector);
 
         // ----- STEP 3 -----
 
         // e) On projete les données centrées sur l'eigenvector
         let norm = eigenvector.norm(); // finding norm of the eigenvector
-        let proj: Vec<Vector3<f64>> = center_points_cloud.iter().map(|point| (point.dot(&eigenvector) / (norm * norm)) * eigenvector).collect();
+        let proj: Vec<Vector3<f64>> = center_points_cloud
+            .iter()
+            .map(|point| (point.dot(&eigenvector) / (norm * norm)) * eigenvector)
+            .collect();
 
         // f) On récupère les deux extrémitées
         let max = *proj.iter().max_by_key(|n| OrderedFloat(n.norm())).unwrap();
@@ -109,7 +116,7 @@ pub fn compute_bones_generation(members: &Vec<(String, Vec<Vector3<f64>>)>) -> V
         // ----- STEP 4 -----
 
         // g) On repositionne les deux points
-        let pc = transform_space(&inv(&barycenter), &center_pc);  // to world space
+        let pc = transform_space(&inv(&barycenter), &center_pc); // to world space
 
         principal_components.push((key.to_string(), pc));
     }
--- backend/src/server.rs
+++ backend/src/server.rs
@@ -12,30 +12,42 @@ pub struct MyBoneGenerator {}
 
 #[tonic::async_trait]
 impl proto::bone_generator_server::BoneGenerator for MyBoneGenerator {
-    async fn execute(&self, request: Request<proto::Members>) -> Result<Response<proto::Members>, Status> {
+    async fn execute(
+        &self,
+        request: Request<proto::Members>,
+    ) -> Result<Response<proto::Members>, Status> {
         println!("Got a request from {:?}", request.remote_addr());
 
         let members = &request.get_ref().members;
 
-        let parsed_request = members.iter()
-                .map(|proto::Member { name, vertices }| {
-                    let points_cloud: Vec<pca::Vector3<f64>> = vertices
-                            .iter()
-                            .map(|vertex| pca::Vector3::new(vertex.x, vertex.y, vertex.z))
-                            .collect();
-                    (name.to_string(), points_cloud)
-                })
-                .collect();
+        let parsed_request = members
+            .iter()
+            .map(|proto::Member { name, vertices }| {
+                let points_cloud: Vec<pca::Vector3<f64>> = vertices
+                    .iter()
+                    .map(|vertex| pca::Vector3::new(vertex.x, vertex.y, vertex.z))
+                    .collect();
+                (name.to_string(), points_cloud)
+            })
+            .collect::<Vec<_>>();
 
         let parsed_reply = pca::compute_bones_generation(&parsed_request)
             .iter()
             .map(|(name, points_cloud)| {
                 let vertices: Vec<proto::Vertex> = points_cloud
-                            .iter()
-                            .map(|point| proto::Vertex { x: point.x, y: point.y, z: point.z })
-                            .collect();
-                proto::Member { name: name.to_string(), vertices }
-            }).collect();
+                    .iter()
+                    .map(|point| proto::Vertex {
+                        x: point.x,
+                        y: point.y,
+                        z: point.z,
+                    })
+                    .collect();
+                proto::Member {
+                    name: name.to_string(),
+                    vertices,
+                }
+            })
+            .collect();
 
         let reply = proto::Members {
             members: parsed_reply,
@@ -51,7 +63,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bone_generator = MyBoneGenerator::default();
 
     Server::builder()
-        .add_service(proto::bone_generator_server::BoneGeneratorServer::new(bone_generator))
+        .add_service(proto::bone_generator_server::BoneGeneratorServer::new(
+            bone_generator,
+        ))
         .serve(addr)
         .await?;
 
